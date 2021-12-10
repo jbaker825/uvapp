d41bf7f2eab84664f23f07676877347483dc9d24
jbaker825 <jbaker825@gmail.com>
2021-12-10 10:17:57 -0600

    CSS

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useImperativeHandle } from 'react';
 
 const About = () => {
     return (
         <div>
-            Everyone loves the way they look with a nice golden tan. However, prolonged exposure can cause serious harm.
+            Everyone loves the way they look with a nice golden tan. However, prolonged exposure can cause serious harm. That is why having access to your local UV levels is very important. It allows you to assess the situation and know approximately how much time in the sun is optimal.
         </div>
     );
 };
