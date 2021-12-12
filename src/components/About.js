6b30a5e21a0fdca6c8694232602e536704120766
jbaker825 <jbaker825@gmail.com>
2021-12-11 18:37:32 -0600

    logo

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React, { useImperativeHandle } from 'react';
 const About = () => {
     return (
         <div>
-            Everyone loves the way they look with a nice golden tan. However, prolonged exposure can cause serious harm. That is why having access to your local UV levels is very important. It allows you to assess the situation and know approximately how much time in the sun is optimal.
+            Everyone loves the way they look with a nice golden tan. However, prolonged exposure can cause serious harm. That is why having access to your local UV levels is very important. It allows you to assess the situation and know approximately how much time in the sun is optimal. This is the intention of this app. Check UV levels in your area, and with our calculator, determine the best amount of time in the sun for that perfect golden glow.
         </div>
     );
 };
